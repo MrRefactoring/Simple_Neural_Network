e29a92fb5f8e8d5b2ff2962f64aae473fb3687b0
Vlad <MrRefactoring@yandex.ru>
2017-10-03 08:50:38 +0300

    Add files via upload

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,27 @@
+from neural import Neural
+from mnist import MNIST
 
+
+if __name__ == "__main__":
+    neural = Neural()
+    neural.train()
+
+    '''test_images, test_labels = MNIST("./train_data/").load_testing()
+
+    total = len(test_images)
+    valid = 0
+    invalid = []
+
+    for i in range(0, total):
+        img = test_images[i]
+        predicted = neural.analyze_mnist(img)
+        true = test_labels[i]
+        if predicted == true:
+            valid = valid + 1
+        else:
+            invalid.append({"image": img, "predicted": predicted, "true": true})
+
+    print("accuracy {}".format(valid / total * 100))'''
+    print(neural.analyze("samples/on.png"))
+    print(neural.analyze("samples/one.png"))
+    print(neural.analyze("samples/seven.png"))
---
+++ Python/neural.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+from PIL import Image
+from mnist import MNIST
+
+
+class Neural:
+    def __init__(self):
+        self.frame = (500, 500)
+        self.coefficients = np.ones((10, 784), np.float32) - .99
+
+    def analyze(self, image_directory):
+        """
+        :param image_directory: путь до картинки, которую необходимо загрузить
+        :return: предсказание нейросети
+        """
+        results = np.zeros(10, np.float32)  # Массив из нулей, который хранит значения анализа для каждой из 10 цифр
+        image = self.__preprocessor(image_directory)  # Загружаем картинку
+
+        for i in range(len(results)):
+            results[i] = np.maximum(np.sum(np.dot(self.coefficients[i], image)), 0)
+        return np.argmax(results)
+
+    def analyze_mnist(self, image):
+        results = np.zeros(10, np.float32)
+        for i in range(len(results)):
+            results[i] = np.maximum(np.sum(np.dot(self.coefficients[i], image)), 0)
+        return np.argmax(results)
+
+    def antiravel(self, image):
+        array = list()
+        for i in range(len(image) // 28):
+            array.append(image[28 * i: 28 * i + 28])
+        return array
+
+    def train(self):
+        images, labels = MNIST("./train_data/").load_training()
+        images = self.__normalize(images)  # Проводим нормализацию изображений
+        for i in range(len(labels)):
+            image = images[i]
+            label = labels[i]
+            prediction = self.analyze_mnist(image)
+            if prediction != label:
+                self.coefficients[prediction] -= image
+                self.coefficients[label] += image
+
+    def __preprocessor(self, image_directory):
+        """
+        :param image_directory: путь до картинки, которую необходимо обработать
+        :return: возвращает плоский numpy array
+        """
+        # Читаем картинку из файла в image, после чего пропускаем ее через ЧБ фильтр
+        image = Image.open(image_directory).convert("L").point(self.__bw_filter(1))
+        return self.__square(np.asarray(image))
+
+    @staticmethod
+    def __bw_filter(threshold=127):  # Черно-белый фильтр с порогом threshold
+        """
+        :param threshold: пороговое значение, после которого выставляется белый цвет
+        :return: возвращает ф-ю, которая вызываестся внутри PIL
+        """
+        def table_gen(x):
+            return 0 if x < threshold else 1
+        return table_gen
+
+    @staticmethod
+    def __normalize(array):
+        for i in range(len(array)):
+            array[i] = np.array(array[i], np.float32) / 255
+        return array
+
+    def __square(self, image):
+        """
+        Метод для преобразования картинки в одномерный массив, размером 784.
+        Сначала просто переводим до размера NxN, потом resize до 28x28
+        После чего делаем массив плоским
+        :param image: numpy массив, картинка
+        :return: image as 784 1D array
+        """
+        if len(image) == len(image[0]):  # Если изображение уже квадратное
+            if len(image) == 28:  # Если изображение уже 28x28 пикселей
+                return image.ravel()  # Возвращаем плоский массив
+            else:
+                return cv2.resize(image, (28, 28)).ravel()  # Делаем resize картинки и возвращаем ее
+        else:
+            max_side_size = max(len(image), len(image[0]))  # Ищем максимально большую сторону
+            # Создаем новую картинку разммером max_size x max_side
+            new_image = np.zeros((max_side_size + self.frame[0], max_side_size + self.frame[1]), np.float32)
+            vertical_indent = (max_side_size - len(image) + self.frame[0]) // 2  # Находим вертикальный отступ
+            horizontal_indent = (max_side_size - len(image[0]) + self.frame[1]) // 2  # Находим горизонтальный отступ
+            # Преобразовываем картинку под нужный формат
+            for i in range(len(image)):
+                for j in range(len(image[i])):
+                    new_image[i + vertical_indent][j + horizontal_indent] = image[i][j]
+            cv2.imshow("Image", cv2.resize(new_image, (28, 28)))
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            return cv2.resize(new_image, (28, 28)).ravel()  # Делаем resize новой картинки и возвращаем ее
